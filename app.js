cd387c4769328a69aceb6968975cf3e70e08d974
Magdalena Piecuch <magdalena.pie@protonmail.com>
2021-04-23 14:08:21 +0200

    added loader

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+window.addEventListener('load', function () {
+    const loader = document.querySelector('.loader');
+    loader.className += ' hideLoader';
+});
+
 const quoteBtn = document.querySelector('#quoteBtn');
 const quoteAuthor = document.querySelector('#quoteAuthor');
 const quote = document.querySelector('#quote');
